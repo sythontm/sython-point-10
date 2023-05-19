fc7a0361dcc0d09415f0f234291b16adbd52ef45
SYTHON <124038843+sythontm@users.noreply.github.com>
2023-05-19 14:37:57 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,58 @@
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+import os
+APP_ID = os.environ.get("APP_ID")
+APP_HASH = os.environ.get("APP_HASH")
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
+session1 = os.environ.get("TERMUX")
+SESSION1 = os.environ.get("TERMUX")
+token = os.environ.get("TOKEN")
+DEVLOO = os.environ.get("DEVLO")
+
+sython1 = TelegramClient(StringSession(session1), APP_ID, APP_HASH)
+bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=token)
+
+
+session2 = os.environ.get("TERMUXX")
+SESSION2 = os.environ.get("TERMUXX")
+sython2 = TelegramClient(StringSession(session2), APP_ID, APP_HASH)
+
+
+session3 = os.environ.get("TERMUXXX")
+SESSION3 = os.environ.get("TERMUXXX")
+sython3 = TelegramClient(StringSession(session3), APP_ID, APP_HASH)
+
+
+session4 = os.environ.get("TERMUXXXX")
+SESSION4 = os.environ.get("TERMUXXXX")
+sython4 = TelegramClient(StringSession(session4), APP_ID, APP_HASH)
+
+
+session5 = os.environ.get("TERMUXXXXX")
+SESSION5 = os.environ.get("TERMUXXXXX")
+sython5 = TelegramClient(StringSession(session5), APP_ID, APP_HASH)
+
+session6 = os.environ.get("TERMUXSI")
+SESSION6 = os.environ.get("TERMUXSI")
+sython6 = TelegramClient(StringSession(session2), APP_ID, APP_HASH)
+
+session7 = os.environ.get("TERMUXSE")
+SESSION7 = os.environ.get("TERMUXSE")
+sython7 = TelegramClient(StringSession(session2), APP_ID, APP_HASH)
+
+session8 = os.environ.get("TERMUXEI")
+SESSION8 = os.environ.get("TERMUXEI")
+sython8 = TelegramClient(StringSession(session2), APP_ID, APP_HASH)
+
+session9 = os.environ.get("TERMUXNI")
+SESSION9 = os.environ.get("TERMUXNI")
+sython9 = TelegramClient(StringSession(session2), APP_ID, APP_HASH)
+
+session10 = os.environ.get("TERMUXTE")
+SESSION10 = os.environ.get("TERMUXTE")
+sython10 = TelegramClient(StringSession(session2), APP_ID, APP_HASH)
+
+ispay = ['yes']
+ispay2 = ['yes']
+bot.start()
+
